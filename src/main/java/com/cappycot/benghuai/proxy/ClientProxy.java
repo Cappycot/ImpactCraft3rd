6b415722ffa3cdafdf6bab5dbdf307707e09de0e
Chris Wang <chriswang444@gmail.com>
2019-06-02 21:49:33 -0500

    Changed constant class names

--- src/main/java/com/cappycot/benghuai/proxy/ClientProxy.java
+++ src/main/java/com/cappycot/benghuai/proxy/ClientProxy.java
@@ -1,6 +1,6 @@
 package com.cappycot.benghuai.proxy;
 
-import com.cappycot.benghuai.HonkaiStrings;
+import com.cappycot.benghuai.HonkaiValues;
 import com.cappycot.benghuai.ImpactCraft;
 
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
@@ -12,7 +12,7 @@ public class ClientProxy extends CommonProxy {
 	@Override
 	public void registerItemRenderer(Item item, int metadata, String id) {
 		ModelLoader.setCustomModelResourceLocation(item, metadata,
-				new ModelResourceLocation(HonkaiStrings.MODID + ":" + id, "inventory"));
+				new ModelResourceLocation(HonkaiValues.MODID + ":" + id, "inventory"));
 	}
 
 }
