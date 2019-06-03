659a4410d347f96f0576e9d1ef393fbd80cac1de
Chris Wang <chriswang444@gmail.com>
2019-06-02 21:48:45 -0500

    Add Nuada's Grief

--- src/main/java/com/cappycot/benghuai/item/ItemRegistry.java
+++ src/main/java/com/cappycot/benghuai/item/ItemRegistry.java
@@ -1,6 +1,6 @@
 package com.cappycot.benghuai.item;
 
-import com.cappycot.benghuai.HonkaiStrings;
+import com.cappycot.benghuai.HonkaiValues;
 
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
@@ -12,15 +12,19 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 @EventBusSubscriber
 public class ItemRegistry {
 
-	public static final ItemTemplate CRYSTAL = new ItemCrystal(HonkaiStrings.CRYSTAL).setCreativeTab(CreativeTabs.MATERIALS);
-	public static final ItemTemplate CRYSTAL_SHARD = new ItemCrystalShard(HonkaiStrings.CRYSTAL_SHARD).setCreativeTab(CreativeTabs.MATERIALS);
-	public static final ItemRaikiri RAIKIRI = new ItemRaikiri(ToolMaterial.DIAMOND, HonkaiStrings.RAIKIRI);
+	public static final ItemTemplate CRYSTAL = new ItemCrystal(HonkaiValues.CRYSTAL)
+			.setCreativeTab(CreativeTabs.MATERIALS);
+	public static final ItemTemplate CRYSTAL_SHARD = new ItemCrystalShard(HonkaiValues.CRYSTAL_SHARD)
+			.setCreativeTab(CreativeTabs.MATERIALS);
+	public static final ItemNuadasGrief NUADAS_GRIEF = new ItemNuadasGrief(HonkaiValues.NUADAS_GRIEF);
+	public static final ItemRaikiri RAIKIRI = new ItemRaikiri(HonkaiValues.RAIKIRI);
 
 	@SubscribeEvent
 	public static void registerItems(RegistryEvent.Register<Item> event) {
-		event.getRegistry().registerAll(CRYSTAL, CRYSTAL_SHARD, RAIKIRI);
+		event.getRegistry().registerAll(CRYSTAL, CRYSTAL_SHARD, NUADAS_GRIEF, RAIKIRI);
 		CRYSTAL.registerItemModel();
 		CRYSTAL_SHARD.registerItemModel();
+		NUADAS_GRIEF.registerItemModel();
 		RAIKIRI.registerItemModel();
 	}
 }
