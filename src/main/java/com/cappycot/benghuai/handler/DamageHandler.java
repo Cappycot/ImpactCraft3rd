42240d7ef2d548405bccc9a694cd462f613e5a1a
Chris Wang <chriswang444@gmail.com>
2019-06-05 00:05:16 -0500

    Prevent damage to friendly entities with sweeping blade

--- src/main/java/com/cappycot/benghuai/handler/DamageHandler.java
+++ src/main/java/com/cappycot/benghuai/handler/DamageHandler.java
@@ -2,6 +2,7 @@ package com.cappycot.benghuai.handler;
 
 import com.cappycot.benghuai.entity.EntityRaikiriSwords;
 import com.cappycot.benghuai.item.HonkaiWeapon;
+import com.cappycot.benghuai.util.Alliance;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
@@ -9,6 +10,7 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.DamageSource;
 import net.minecraftforge.event.entity.living.LivingAttackEvent;
+import net.minecraftforge.event.entity.living.LivingKnockBackEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -19,7 +21,7 @@ public class DamageHandler {
 	/**
 	 * Mitigate damage invulnerability time with Honkai weapons.
 	 */
-	@SubscribeEvent(priority = EventPriority.LOWEST)
+	@SubscribeEvent
 	public static void onLivingAttack(LivingAttackEvent event) {
 		EntityLivingBase entity = event.getEntityLiving();
 		DamageSource source = event.getSource();
@@ -27,9 +29,14 @@ public class DamageHandler {
 		if (trueSource instanceof EntityPlayer) {
 			EntityPlayer player = (EntityPlayer) trueSource;
 			ItemStack itemStack = player.getHeldItemMainhand();
-			if (itemStack != null && itemStack.getItem() instanceof HonkaiWeapon) {
-				entity.hurtResistantTime = 0;
-				entity.hurtTime = 0;
+			if (itemStack.getItem() instanceof HonkaiWeapon) {
+				// Have to do that last check because of sweeping sword damage.
+				if (Alliance.isAllyOfPlayer(entity, player))
+					event.setCanceled(true);
+				else {
+					entity.hurtResistantTime = 0;
+					entity.hurtTime = 0;
+				}
 			}
 		}
 	}
