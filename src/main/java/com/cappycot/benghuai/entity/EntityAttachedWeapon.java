46e11f8190fa2c68b9be0f7b658c98d058dee14c
Chris Wang <chriswang444@gmail.com>
2020-03-27 17:08:50 -0500

    Placeholder class

---
+++ src/main/java/com/cappycot/benghuai/entity/EntityAttachedWeapon.java
@@ -0,0 +1,25 @@
+package com.cappycot.benghuai.entity;
+
+import java.util.UUID;
+
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.world.World;
+
+/**
+ * A summoned entity that is attached to some living entity.
+ */
+public abstract class EntityAttachedWeapon extends EntityHonkaiWeapon {
+
+	private EntityLivingBase attached = null;
+	private UUID attachedUUID = null;
+
+	public EntityAttachedWeapon(World world) {
+		super(world);
+	}
+
+	public EntityAttachedWeapon(World world, EntityPlayer owner) {
+		super(world, owner);
+	}
+
+}
