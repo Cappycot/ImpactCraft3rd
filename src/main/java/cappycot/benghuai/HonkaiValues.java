41485d704b6f95c4b011bb208d969867415c8232
Cappycot <chriswang444@gmail.com>
2020-05-08 01:33:48 -0500

    Bump version to 0.2.1

--- src/main/java/cappycot/benghuai/HonkaiValues.java
+++ src/main/java/cappycot/benghuai/HonkaiValues.java
@@ -7,7 +7,7 @@ public class HonkaiValues {
 	// Mod Information
 	public static final String MODID = "impactcraft";
 	public static final String NAME = "ImpactCraft 3rd";
-	public static final String VERSION = "1.12.2-0.2.0";
+	public static final String VERSION = "1.12.2-0.2.1";
 	// Item Names
 	public static final String CRYSTAL = "crystal";
 	public static final String CRYSTAL_SHARD = "crystal_shard";
